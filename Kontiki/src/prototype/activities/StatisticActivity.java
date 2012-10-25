a8ce1045cd7a7b95012ddca07342c9859a8d3732
ag12 <amirg12@hotmail.no>
2012-10-25 16:04:32 +0200

    listener

--- Kontiki/src/prototype/activities/StatisticActivity.java
+++ Kontiki/src/prototype/activities/StatisticActivity.java
@@ -1,22 +1,36 @@
 package prototype.activities;
 
-
-
 import prototype.adapter.KontikiArrayadapter;
 import prototype.start.R;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class StatisticActivity extends Activity {
 
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_statistic);
-        
-        ListView statisticsListView = (ListView) findViewById(R.id.statistics_list);
+	private Button overAllStatisticButton;
+	private OnClickListener oasButtonListener = new OnClickListener() {
+
+		@Override
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
+			Toast.makeText(
+					StatisticActivity.this,
+					"OAS button..", 10).show();
+		}
+	};
+
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_statistic);
+
+		ListView statisticsListView = (ListView) findViewById(R.id.statistics_list);
 		String[] books = new String[] { "The Lord of the Rings",
 				"Harry Potter and the Goblet of Fire",
 				"The Leonardo da Vinci Code", "Teenage Mutant Ninja Turtles",
@@ -28,11 +42,14 @@ public class StatisticActivity extends Activity {
 		ArrayAdapter<String> statisticsAdapter = new KontikiArrayadapter(this,
 				books, R.layout.row_layout_statistics, R.drawable.human);
 		statisticsListView.setAdapter(statisticsAdapter);
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_tab_host, menu);
-        return true;
-    }
+		overAllStatisticButton = (Button) findViewById(R.id.button_statistics);
+		overAllStatisticButton.setOnClickListener(oasButtonListener);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.activity_tab_host, menu);
+		return true;
+	}
 }
