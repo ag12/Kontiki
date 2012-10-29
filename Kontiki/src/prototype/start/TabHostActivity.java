ddb948ca0af724d812ab8b48bd3dfdd5f926b296
ag12 <amirg12@hotmail.no>
2012-10-29 15:38:20 +0100

    I hate mondays

--- Kontiki/src/prototype/start/TabHostActivity.java
+++ Kontiki/src/prototype/start/TabHostActivity.java
@@ -7,6 +7,7 @@ import prototype.activities.StatisticActivity;
 import android.app.TabActivity;
 import android.content.Intent;
 import android.content.res.Resources;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
 import android.widget.TabHost;
@@ -31,30 +32,38 @@ public class TabHostActivity extends TabActivity {
 		intent = new Intent().setClass(this, HomeActivity.class);
 
 		spec = tabHost.newTabSpec("main")
-				.setIndicator("", res.getDrawable(R.drawable.home))
+				.setIndicator("", res.getDrawable(R.drawable.icon_home_press))
 				.setContent(intent);
 		tabHost.addTab(spec);
 
 		intent = new Intent().setClass(this, BookStoreActivity.class);
 		spec = tabHost.newTabSpec("peoples")
-				.setIndicator("", res.getDrawable(R.drawable.home))
+				.setIndicator("", res.getDrawable(R.drawable.icon_store_press))
 				.setContent(intent);
 		tabHost.addTab(spec);
 
 		intent = new Intent().setClass(this, StatisticActivity.class);
-		spec = tabHost.newTabSpec("everbody")
-				.setIndicator("", res.getDrawable(R.drawable.home))
+		spec = tabHost
+				.newTabSpec("everbody")
+				.setIndicator("", res.getDrawable(R.drawable.icon_social_press))
 				.setContent(intent);
 		tabHost.addTab(spec);
 
 		intent = new Intent().setClass(this, SocialActivity.class);
-		spec = tabHost.newTabSpec("everbody")
-				.setIndicator("", res.getDrawable(R.drawable.home))
+		spec = tabHost
+				.newTabSpec("everbody")
+				.setIndicator("",
+						res.getDrawable(R.drawable.icon_statistics_press))
 				.setContent(intent);
 		tabHost.addTab(spec);
 
 		// tabHost.getTabWidget().getChildAt(3).setVisibility(View.GONE);
-
+		// tabHost.getTabWidget().getChildAt(3).seth
+		for (int i = 0; i < tabHost.getTabWidget().getChildCount(); i++) {
+			tabHost.getTabWidget().getChildAt(i)
+					.setBackgroundColor(Color.parseColor("#3BB9FF")); // unselected287EAC  329BD4
+		}
+	
 		tabHost.setCurrentTab(tabHost.getCurrentTab());
 
 	}
