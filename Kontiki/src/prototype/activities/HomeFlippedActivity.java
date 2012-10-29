60a330d9118c8b0676395cc98484944e6ad9346d
Patrick Romstad <promstad1@gmail.com>
2012-10-29 19:42:14 +0100

    fixed flipping

--- Kontiki/src/prototype/activities/HomeFlippedActivity.java
+++ Kontiki/src/prototype/activities/HomeFlippedActivity.java
@@ -76,9 +76,16 @@ public class HomeFlippedActivity extends Activity {
 		coverFlow.setAnimationDuration(1000);
 
 		setContentView(coverFlow);
-		TabActivity ta = (TabActivity) this.getParent();
-		ta.getTabWidget().getChildAt(4).setVisibility(View.VISIBLE);
-		ta.getTabHost().setCurrentTab(4);
+//		TabActivity ta = (TabActivity) this.getParent();
+//		ta.getTabWidget().getChildAt(4).setVisibility(View.VISIBLE);
+//		ta.getTabHost().setCurrentTab(4);
+	}
+	
+	@Override
+	public void onConfigurationChanged(Configuration newConfig) {
+		super.onConfigurationChanged(newConfig);
+		Intent flipped = new Intent(this, TabHostActivity.class);
+		startActivity(flipped);
 	}
 
 	public class ImageAdapter extends BaseAdapter {
